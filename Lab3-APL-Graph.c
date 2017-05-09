44ae390bb40e97df90fff05d936e13bb1f8f555a
Gabriel <gabriel.brito55@gmail.com>
2017-05-09 01:51:42 -0300

    Passagem de parâmetros e inserção funcionando

--- Lab3-APL-Graph.c
+++ Lab3-APL-Graph.c
@@ -21,16 +21,30 @@ int buffer[N];
 int count;
 int last_produced_item;
 int last_consumed_item;
+int ** removed_item;
+//Buffer para guardar os grafos ja construidos
+int ** graph_buffer[N];
 
 void insert_item(int item) {
 	debug("Inserting item %d",item);
 	buffer[end]=item;
+	printf("end = %d\n", end);
 	end=(end+1)%N;
 	debugtxt("Item inserted!");
 }
 int remove_item() {
 	debugtxt("Removing item ...");
 	int item = buffer[start];
+	int i,j;
+	//Copia o primeiro elemento
+	for(i=0;i<N_VERT;i++){
+		for (j = 0; j < N_VERT; j++){
+			//printf("i=%d, j=%d\n",i,j);
+			//printf("graph_buffer[start][%d][%d] = %d\n",i,j,graph_buffer[start][i][j]);
+			removed_item[i][j] = graph_buffer[start][i][j];
+		}
+	}
+	
 	start=(start+1)%N;
 	debug("Item %d removed!",item);
 	return item;
@@ -68,7 +82,7 @@ void _down(sem_t *sem, const char * name) {
 }
 
 // Produtor e consumidor ...
-int produce_item(int adj_matrix[N_VERT][N_VERT]) {
+int produce_item(int **adj_matrix) {
 	debugtxt("Producing item ...");
 	float z=1.2;
 	float q;
@@ -109,23 +123,20 @@ void* producerFunc() {
 	debugtxt("Starting producer");
 	int item;
 	int **mat;
-	mat = malloc(N_VERT*sizeof(int));
-	int i,j;
-	for (i = 0; i < N_VERT; ++i)
-	{
-		mat[i] = malloc(N_VERT*sizeof(int));
-	}
 	while(TRUE) {
-		item=produce_item(mat);
 		down(&empty);
-		//sem_wait(&empty);
 		down(&mutex);
-		//sem_wait(&mutex);
+		//A produção do grafo já modifica o buffer
+		item=produce_item(graph_buffer[end]);
+		//int i,j;
+		//for(i=0;i<N_VERT;i++){
+		//	for(j=0;j<N_VERT;j++){
+		//		printf("graph_buffer[end][%d][%d] = %d\n",i,j, graph_buffer[end][i][j]);
+		//	}
+		//}
 		insert_item(item);
 		up(&mutex);
-		//sem_post(&mutex);
 		up(&full);
-		//sem_post(&full);
 	}
 	debugtxt("Ending producer");
 	return 0;
@@ -151,13 +162,24 @@ void* consumerFunc() {
 }
 
 int main() {
-	int i;
-
 	last_produced_item = 0;
 	start = 0;
 	end = 0;
 	int err;
 	void* status;
+	int i,j;
+	for(j=0; j<N; j++){
+		graph_buffer[j] = (int **) malloc(N_VERT*sizeof(int *));
+		for (i = 0; i < N_VERT; i++)
+		{
+			graph_buffer[j][i] = (int *) malloc(N_VERT*sizeof(int));
+		}
+	}
+
+	removed_item = 	(int **) malloc(N_VERT*sizeof(int *));
+	for (i = 0; i < N_VERT; i++){
+		removed_item[i] = (int *) malloc(N_VERT*sizeof(int));
+	}
 	// Criando semaforos ...
 	/*
 	full = CreateSemaphore( 
